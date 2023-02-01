84b322ba8c139f672c62fcec20ff292855d2606c
David <David.arcos1000@gmail.com>
2023-01-31 22:36:08 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function nombre() {     
+    const nombre = document.getElementById("nombre").value;
+    document.getElementById("mensaje").innerHTML = ("Hola "+ nombre + ", Gracias por suscribirte");
+}
